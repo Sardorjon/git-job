b49e6a9709608c40b5ae9b7693c3292876f2cfe1
Sardorjon <sardortemirov120@gmail.com>
2018-05-27 20:52:25 +0200

    large-hero section is almost done

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('html', function(){
     console.log("Hooray - you created a html task");
 });
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
